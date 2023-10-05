c61428d1fcdf37463c8ea87787d881f4d0898267
vakkun <vaktus1@gmail.com>
2023-10-04 23:51:30 -0700

    Printf.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.io>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
