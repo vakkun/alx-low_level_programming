f912799e9a66ed548b44f4495181a7c7a748714a
vakkun <vaktus1@gmail.com>
2023-10-05 01:05:35 -0700

    Quote.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-	char msg[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, msg, sizeof(msg) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
