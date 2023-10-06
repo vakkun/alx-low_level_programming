b09d9ca9e7508540b201ff851a7ac7adfcefc9fb
vakkun <vaktus1@gmail.com>
2023-10-06 05:07:23 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * void - empty
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	for (char 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchatr('\n');
+	return (0);
+}
