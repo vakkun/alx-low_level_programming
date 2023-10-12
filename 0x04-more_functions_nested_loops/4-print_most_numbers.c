7596822033477a92a597a4a4a4f935527d262fb1
vakkun <vaktus1@gmail.com>
2023-10-12 05:23:15 -0700

    Print most numbers.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,17 +4,17 @@
  * print_most_numbers - prints the numbers, from 0 to 9,
  * except 2 and 4, followed by a new line
  */
-
 void print_most_numbers(void)
 {
-    int n;
+	int i;
 
-    for (n = 0; n < 10; n++)
-    {
-        if (n != 2 && n != 4)
-        {
-            _putchar(n + '0');
-        }
-    }
-    _putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
 }
