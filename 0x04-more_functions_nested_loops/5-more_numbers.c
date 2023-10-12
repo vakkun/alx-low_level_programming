a9b781d95c1c966a1b48f0bfe38cb35cad3293f6
vakkun <vaktus1@gmail.com>
2023-10-12 05:40:40 -0700

    More numbers.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,16 +4,18 @@
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
-
 void more_numbers(void)
 {
-    int n = 0, m;
-    while (n < 10)
-    {
-        for (m = 0; m < 15; m++)
-        {
-            _putchar(m + '0');
-        }
-    _putchar('\n');
-    }
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
