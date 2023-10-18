79a8ff4af7d9e6012d608f5c424318a67625636c
vakkun <vaktus1@gmail.com>
2023-10-18 11:28:10 -0700

    Print number.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: number to be printed
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+	int power, neg, hold;
+
+	neg = 0;
+	power = 1;
+	hold = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		neg = 1;
+	}
+	while (hold > 9 || hold < -9)
+	{
+		power *= 10;
+		hold /= 10;
+	}
+	while (power > 0)
+	{
+		if (power > 9)
+		{
+			if (!neg)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+
+			power /= 10;
+		}
+		if (power == 1)
+		{
+			if (neg)
+				_putchar((n % 10) * -1 + '0');
+			else
+				_putchar(n % 10 + '0');
+			power = 0;
+		}
+	}
+}
