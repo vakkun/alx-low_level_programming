e7ba8cc1377495282293d6a204377b30306ba358
vakkun <vaktus1@gmail.com>
2023-10-25 18:25:53 -0700

    Puts recursion.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string with new line
+ * @s: printed character
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
