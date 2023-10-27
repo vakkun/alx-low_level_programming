d54e874eff26ef6c50fddcbaa0b5763da80c2fed
vakkun <vaktus1@gmail.com>
2023-10-27 09:29:35 -0700

    Change.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,30 +10,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int cent_vals[] = {25, 10, 5, 2, 1};
-	int cents, number_of_cents, balance, i;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-			return (1);
+		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	number_of_cents = 0;
-	balance = cents;
-	for (i = 0; i < 5; ++i)
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		number_of_cents += balance / cent_vals[i];
-		if ((balance % cent_vals[i]) == 0)
-			break;
-		balance %= cent_vals[i];
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
 	}
-	printf("%d\n", number_of_cents);
+
+	printf("%d\n", result);
 	return (0);
 }
